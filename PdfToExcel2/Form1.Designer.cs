fcbf1b80a2913274355a2d1f391cf2417c23175e
max_p <max_pechin@mail.ru>
2022-07-05 21:24:55 +0300

    Добавьте файлы проекта.

---
+++ PdfToExcel2/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace PdfToExcel2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выберите Файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(22, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(519, 24);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 119);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Location = new System.Drawing.Point(1000, 0);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Button button1;
+    }
+}
---
+++ PdfToExcel2/Form1.cs
@@ -0,0 +1,158 @@
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+//using System;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+
+namespace PdfToExcel2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "PDF Files|*.pdf";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            button1.Enabled = false;
+            var TextList = GetText(filePath);
+            for (int i = 0; i < TextList.Count; i++)
+            {
+                string L = "";
+                foreach (var x in TextList[i].Split("\n"))
+                    L = L + x;
+                TextList[i] = L;
+                int a = (TextList[i]).Length;
+            }
+            string name = GetFileName(filePath);
+            string path = GetFilePath(filePath);
+
+            SaveToExcel(TextList, path, name);
+            button1.Enabled = true;
+        }
+
+        private void SaveToExcel(List<string> Text, string path, string name)
+        {
+            Excel.Application oXL;
+            Excel._Workbook oWB;
+            Excel._Worksheet oSheet;
+            Excel.Range oRng;
+
+            try
+            {
+                //Start Excel and get Application object.
+                oXL = new Excel.Application();
+                oXL.Visible = true;
+
+                //Get a new workbook.
+                oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
+                oSheet = (Excel._Worksheet)oWB.ActiveSheet;
+
+                int N = Text.Count;
+                for (int i=1; i<N+1; i++)
+                {
+                    oSheet.Cells[i, 1] = Text[i-1];
+                    textBox1.Text = ((i + 1).ToString() + "/" + N.ToString() + ": " + Text[i-1]);
+                }
+
+                textBox1.Text = "Записано";
+
+
+            }
+            catch (Exception theException)
+            {
+                String errorMessage;
+                errorMessage = "Error: ";
+                errorMessage = String.Concat(errorMessage, theException.Message);
+                errorMessage = String.Concat(errorMessage, " Line: ");
+                errorMessage = String.Concat(errorMessage, theException.Source);
+
+                MessageBox.Show(errorMessage, "Error");
+            }
+
+
+        }
+
+        private List<string> GetText(string path)
+        {
+            var res = new List<string>();
+            using (PdfReader reader = new PdfReader(path))
+            {
+                StringBuilder text = new StringBuilder();
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    res.Add(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+
+                return res;
+            }
+        }
+
+        private string GetFileName(string path)
+        {
+            string res = "";
+            res = path.Split('\\')[path.Split('\\').Length - 1];
+            res = res.Replace(".pdf", "");
+            return res;
+        }
+
+        private string GetFilePath(string path)
+        {
+            return path.Substring(0, path.LastIndexOf('\\'));
+        }
+
+        private string GenerateName(string path, string name)
+        {
+            string res = "";
+            var directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            List<string> FileNames = new List<string>();
+            foreach (FileInfo file in files)
+            {
+                FileNames.Add(file.Name);
+            }
+            if (!FileNames.Contains(name + ".xlsx"))
+                return name;
+            int n = 1;
+            while (FileNames.Contains(name + "(" + n.ToString() + ").xlsx"))
+                n++;
+            //string s = name + "(" + n.ToString() + ").xlsx";
+            return (name + "(" + n.ToString() + ")");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
